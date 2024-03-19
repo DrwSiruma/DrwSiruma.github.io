2ae277795578c39aacdcbf093d1707194b5ee8d1
DrwSiruma <andrewsiruma02@gmail.com>
2024-03-19 22:32:40 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Portfolio</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Andrew Siruma</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#certifications">Certifications</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Andrew
+                        <span class="text-primary">Siruma</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Bacoor City · Cavite, 4102 · (+63) 995-763-6697 ·
+                        <a href="mailto:andrewsiruma02@gmail.com">andrewsiruma02@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">I have 2 years of experience as a Junior PHP Programmer in the Web Development Industry. My expertise lies in front-end development but I am usually a full-stack developer, where I use HTML5, CSS, and JavaScript to create web applications. Additionally, I have multimedia editing skills and can create logos, posters, templates, and many more using Adobe Photoshop CS6.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" target="_blank" href="https://www.linkedin.com/in/andrew-siruma-51aa9b2a1"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" target="_blank" href="https://github.com/DrwSiruma/DrwSiruma.github.io.git"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" target="_blank" href="https://twitter.com/drewsiruma"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" target="_blank" href="https://www.facebook.com/andrew.siruma.3"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Junior PHP Developer</h3>
+                            <div class="subheading mb-3">System Up Business Solutions Inc.</div>
+                            <p>As a Junior PHP Developer at System Up, I played a vital role in the creation, enhancement, and support of various web applications. My responsibilities encompassed developing robust systems, generating detailed reports, resolving client issues, and refining system functionalities across multiple domains.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">June 2022 - March 2024</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Data Scientist</h3>
+                            <div class="subheading mb-3">Metro Pacific Tollways Corporation</div>
+                            <p>As a Data Extraction Specialist, my role encompassed efficiently gathering data from diverse systems and sources. Responsibilities included downloading data from internal and external platforms, ensuring accuracy and completeness, and uploading it to designated servers with meticulous organization. I compiled comprehensive lists of extracted data, documenting key details and creating guides for easy reference. Additionally, I conducted quality assurance checks, verifying data integrity and consistency, while tracking extraction progress to report to stakeholders. Throughout, I maintained a high standard of accuracy and organization, facilitating smooth data extraction processes.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">January 2024 - March 2024</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Cavite State University - Bacoor City Campus</h3>
+                            <div class="subheading mb-3">Bachelor of Science</div>
+                            <div>Computer Science</div>
+                            <!-- <p>GPA: 3.23</p> -->
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">August 2018 - August 2022</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Asian Institute of Computer Studies - Bacoor</h3>
+                            <div class="subheading mb-3">Information and Communication Technology</div>
+                            <!-- <p>GPA: 3.56</p> -->
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2016 - 2018</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-bootstrap"></i></li> 
+                        <!-- <li class="list-inline-item"><i class="fab fa-angular"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-react"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-node-js"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-less"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-wordpress"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-gulp"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-grunt"></i></li> -->
+                        <!-- <li class="list-inline-item"><i class="fab fa-npm"></i></li> -->
+                    </ul>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Projects-->
+            <section class="resume-section" id="projects">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Projects</h2>
+                    <div class="m-container">
+                        <div class="m-box">
+                            <img src="assets/img/BRGY_SYSTEM.PNG" class="m-img" alt="Image">
+                            <div class="m-box-content">
+                            <div class="m-title">Brgy. 905 Management System</div>
+                            <button class="m-learn-more">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="m-box">
+                            <img src="assets/img/STA_LUCIA.PNG" class="m-img" alt="Image">
+                            <div class="m-box-content">
+                            <div class="m-title">Sta Lucia School Pre-Enrollment with Report List System</div>
+                            <button class="m-learn-more">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="m-box">
+                            <img src="assets/img/LOCAL_STREETWARE.PNG" class="m-img" alt="Image">
+                            <div class="m-box-content">
+                            <div class="m-title">Local Streetware Inventory and Ordering System</div>
+                            <button class="m-learn-more">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="m-box">
+                            <img src="assets/img/DIGITAL_MUSEUM.PNG" class="m-img" alt="Image">
+                            <div class="m-box-content">
+                            <div class="m-title">Digital Museum of Artifacts & History for the Municipality of Indang Cavite</div>
+                            <button class="m-learn-more">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="m-box">
+                            <img src="assets/img/ROYANA_RESORT.PNG" class="m-img" alt="Image">
+                            <div class="m-box-content">
+                            <div class="m-title">Royana Hotel & Resort Reservation System</div>
+                            <button class="m-learn-more">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="m-box">
+                            <img src="assets/img/CLUTCH_TIME.PNG" class="m-img" alt="Image">
+                            <div class="m-box-content">
+                            <div class="m-title">Clutch Time POS & Inventory System</div>
+                            <button class="m-learn-more">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="m-box">
+                            <img src="assets/img/SPARC.PNG" class="m-img" alt="Image">
+                            <div class="m-box-content">
+                            <div class="m-title">Inner SPARC Realty Inc. CRM</div>
+                            <button class="m-learn-more">Learn More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Certifications -->
+            <section class="resume-section" id="certifications">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Computer Literacy Training Program – Unitech Educational Program Corporation 2012
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Multimedia Graphics Motion and Basic Videography 2018
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            NDG Linux Unhatched Course - Cisco Networking Academy 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Networking Essentials - Cisco Networking Academy 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Introduction to Packet Tracer - Cisco Networking Academy 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Introduction to IoT - Cisco Networking Academy 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Introduction to Cyber Security - Cisco Networking Academy 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Get Connected - Cisco Networking Academy 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Cybersecurity Essentials - Cisco Networking Academy 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            A Practical Lesson in Cloud Controls from Breach Analysis - Bright Talk 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            DECODE 2020 – Cybersecurity Fundamentals - TREND MICRO 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            DECODE 2020 – Data Processing: A Skill Everyone Needs - TREND MICRO 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Introduction to Email Threats and Security Attacts - TREND MICRO 2020
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Introduction to Security Threats - TREND MICRO 2020
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
